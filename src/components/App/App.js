c79d9aa36bc0c65c19ca8bf3aeb9e7e846470d9a
KatyaPesnya <katyapesnya@icloud.com>
2021-08-24 22:01:49 +0300

    In production mode, the address of the database is taken from process.env.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+//import Header from '../Header/Header';
+import "./App.css";
 
 function App() {
-    return (
-        <div className="app">
-            <h1>Hello</h1>
-        </div>
-    );
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      <Switch>
+        <Route exact path="/"></Route>
+        <Route path="/movies"></Route>
+        <Route path="/saved-movies"></Route>
+        <Route path="/profile"></Route>
+        <Route path="/signin"></Route>
+        <Route path="/signup"></Route>
+      </Switch>
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
