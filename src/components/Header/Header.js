c94e68418a34daa5a91dff1e3843736bbc335e01
KatyaPesnya <katyapesnya@icloud.com>
2021-09-10 12:25:04 +0300

    adding navlink component header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import LogoLink from '../LogoLink/LogoLink'
-
+import { NavLink } from "react-router-dom";
 
 function Header() {
+
     return (
         <header className="header">
             <LogoLink />
-          <button className="header__button header__button">Регистрация</button>
-            <button className="header__button">Войти</button>
+            
+  <NavLink className="header__button" to="/signup">Регистрация</NavLink>
+<NavLink className="header__button" to="/signin"> Войти</NavLink>
         </header>
     )
 }
