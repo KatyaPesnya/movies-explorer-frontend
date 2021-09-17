ac9d4c40e4029b50c9f953f35428be8bf0a0db66
KatyaPesnya <katyapesnya@icloud.com>
2021-09-17 18:34:49 +0300

    add  alt font-family

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,9 +3,9 @@ import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
 function SearchForm() {
   return (
-    <form className="search-form">
+    <form className="search-form" >
       <div>
-        <input className="search-form__input" type="text" placeholder="Фильм" />
+        <input className="search-form__input" type="text" placeholder="Фильм" required/>
         <button className="search-form__button" type="submit">
           Найти
         </button>
